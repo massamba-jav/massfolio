8280f958fa1bf5e5ec573e33800cc9cd29a7236d
Massamba <masscompte133@gmail.com>
2025-12-23 09:12:15 +0000

    Switch Font Awesome to local assets and update version
    Font Awesome was updated to v7.1.0 and switched from CDN to local assets for improved reliability and performance. Updated package.json dependencies and index.html to reference local CSS, and added required font files. Footer component and related references were updated accordingly.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,4 @@
-import React, {useContext} from "react";
 import "./Footer.scss";
-import {Fade} from "../../utils/Reveal";
-import emoji from "react-easy-emoji";
-import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
   // const {isDark} = useContext(StyleContext);
